44b944bb7cee642c1d29bf48fcf497c92e8512ba
MarcoGrossi92 <marcogrossi@live.it>
2024-11-21 18:57:00 +0100

    add printed text

--- src/fortran/app/main.f90
+++ src/fortran/app/main.f90
@@ -5,6 +5,7 @@ program CEAprogram
   character(len=500) :: filename
 
   CEA%indx = 1
+  write(*,*) "Enter CEA file name (without .inp extension)"
   read(*,*) filename
   call CEA%solve(filename)
 
