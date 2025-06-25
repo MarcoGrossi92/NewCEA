eac571e30a0ad916883903015d5e11c824c604f0
Marco Grossi <50102629+MarcoGrossi92@users.noreply.github.com>
2025-06-26 00:54:39 +0200

    refactor install.sh to improve structure and command handling; enhance usage instructions and error messages

--- install.sh
+++ install.sh
@@ -1,41 +1,40 @@
-#!/bin/bash -
-#===============================================================================
-#
-#          FILE: intstall.sh
-#
-#         USAGE: run "./install.sh [options]" from NewCEA master directory
-#
-#   DESCRIPTION: A utility script that builds NewCEA project
-#===============================================================================
+#!/bin/bash
 
-# DEBUGGING
-set -e
-set -C # noclobber
+set -e  # Exit on any command failure
+set -u  # Treat unset variables as an error
 
-# INTERNAL VARIABLES AND INITIALIZATIONS
-readonly PROJECT="NewCEA"
+PROGRAM=$(basename "$0")
 readonly DIR=$(pwd)
-readonly PROGRAM=`basename "$0"`
+VERBOSE=false
 
-function usage () {
-    echo "Install script of $PROJECT"
-    echo "Usage:"
-    echo
-    echo "$PROGRAM --help|-?"
-    echo "    Print this usage output and exit"
-    echo
-    echo "$PROGRAM --build  |-b"
-    echo "    Build the project and libraries via setup.py"
-    echo
-    echo "$PROGRAM --compile|-c <build>"
-    echo "    Compile the Fortran sources (<build> = RELEASE,DEBUG,TESTING)"
-    echo
-    echo "$PROGRAM --full-compile|-f <build>"
-    echo "    Compile the Fortran sources and update the python extension (<build> = RELEASE,DEBUG,TESTING)"
-    echo
-    echo "$PROGRAM --setvars|-s"
-    echo "    Set the project paths in the environment variables"
-    echo
+function usage() {
+    cat <<EOF
+
+Install script for NewCEA
+
+Usage:
+  $PROGRAM [GLOBAL_OPTIONS] COMMAND [COMMAND_OPTIONS]
+
+Global Options:
+  -h       , --help         Show this help message and exit
+  -v       , --verbose      Enable verbose output
+
+Commands:
+  build                     Perform a full build
+
+  compile                   Compile the program using the CMakePresets file
+
+  setvars                   Set project paths in environment variables
+
+EOF
+    exit 1
+}
+
+
+function log() {
+    if [ "$VERBOSE" = true ]; then
+        echo "$1"
+    fi
 }
 
 
@@ -53,125 +52,83 @@ function define_path () {
   source $RCFILE --force
 }
 
-function compile () {
-  echo -e "\033[0;34mBuilding Fortran sources via CMake...\033[0m"
-  mkdir -p $DIR/build
-  cd $DIR/build
-  cmake .. -DCMAKE_BUILD_TYPE=$TYPE
-  make
-}
 
-function full_compile () {
-  rm -rf src/python/NewCEA/FCEA2*
-  compile
+# Default global values
+COMMAND=""
+BUILD_TYPE="RELEASE"
 
-  echo -e "\033[0;34mBuilding Python extension with f2py...\033[0m"
-  cd $DIR
-  cd src/python/NewCEA
-  f2py -c -m FCEA2 ../../fortran/lib/CEAinc.f90 ../../fortran/lib/cea2.f
-  mv FCEA2* FCEA2.so
-}
-
-function build_project () {
-  rm -rf bin build
-
-  echo 
-  echo -e "\033[0;32mNewCEA building \033[0m"
-  echo
-  # Determine the correct pip command
-  if command -v pip3 &> /dev/null; then
-      PIP_CMD="pip3"
-  elif command -v pip &> /dev/null; then
-      PIP_CMD="pip"
-  else
-      echo -e "\033[0;31mError: pip is not installed on this system.\033[0m"
-      exit 1
-  fi
-
-  full_compile
-
-  # Install the package using pip
-  echo -e "\033[0;34mInstalling NewCEA package...\033[0m"
-  $PIP_CMD install -e . $VERBOSE
-  cd $DIR
-  echo -e "\033[0;32mInstallation completed successfully.\033[0m"
-
-}
-
-VERBOSE=F
-TYPE=F
-SETVARS=F
-COMPILE=F
-FCOMPILE=F
-BUILD=F
-
-# PROCESS COMMAND-LINE ARGUMENTS
-if [[ $# -eq 0 ]]; then
-  usage
-fi
-
-while test $# -gt 0; do
-  if [ x"$1" == x"--" ]; then
-    # detect argument termination
-    shift
-    break
-  fi
-  case $1 in
-
-    --build | -b )
-      shift
-      BUILD=T
-      TYPE="RELEASE"
-      ;;
-
-    --compile | -c )
-      shift
-      COMPILE=T
-      TYPE="$1"
-      ;;
-
-    --full-compile | -f )
-      shift
-      FCOMPILE=T
-      TYPE="$1"
-      ;;
-
-    --setvars | -s )
-      shift
-      SETVARS=T
-      ;;
-
-    --verbose | -v )
-      shift
-      VERBOSE="--verbose"
-      ;;
-
-    -? | --help )
-      usage
-      exit
-      ;;
-
-    -* )
-      echo "Unrecognized option: $1" >&2
-      usage
-      exit $E_BAD_OPTION
-      ;;
-
-    * )
-      break
-      ;;
-  esac
+# Parse options with getopts
+while getopts "hv:-:" opt; do
+    case "$opt" in
+        -)
+            case "$OPTARG" in
+                verbose) VERBOSE=true ;;
+                help) usage ;;
+                *) echo "Error: Unknown global option '--$OPTARG'"; usage ;;
+            esac
+            ;;
+        h) usage ;;
+        v) VERBOSE=true ;;
+        *) echo "Error: Unknown global option '-$opt'"; usage ;;
+    esac
 done
+shift $((OPTIND -1))
 
-if [ "$SETVARS" != "F" ]; then
-  define_path
-elif [[ "$BUILD" != "F" ]]; then
-  define_path
-  build_project
-elif [[ "$COMPILE" != "F" ]]; then
-  compile
-elif [[ "$FCOMPILE" != "F" ]]; then
-  full_compile
-else
-  usage
+# Ensure a command was provided
+if [[ $# -eq 0 ]]; then
+    echo "Error: No command provided!"
+    usage
 fi
+
+COMMAND="$1"
+shift
+
+# Execute the selected command
+case "$COMMAND" in
+    build)
+        rm -rf bin build
+        # Determine the correct pip command
+        if command -v pip3 &> /dev/null; then
+            PIP_CMD="pip3"
+        elif command -v pip &> /dev/null; then
+            PIP_CMD="pip"
+        else
+            echo -e "\033[0;31mError: pip is not installed on this system.\033[0m"
+            exit 1
+        fi
+
+        # Compile the project
+        cmake -B build -DCMAKE_BUILD_TYPE=$BUILD_TYPE || exit 1
+        cmake --build build || exit 1
+
+        # Ccompile the dynamic lib
+        echo -e "\033[0;34mBuilding Python extension with f2py...\033[0m"
+        cd src/python/NewCEA
+        f2py -c -m FCEA2 ../../fortran/lib/CEAinc.f90 ../../fortran/lib/cea2.f
+        mv FCEA2* FCEA2.so
+
+        # Install the package using pip
+        echo -e "\033[0;34mInstalling NewCEA package...\033[0m"
+        $PIP_CMD install -e . $VERBOSE
+        cd $DIR
+        echo -e "\033[0;32mInstallation completed successfully.\033[0m"
+        ;;
+    compile)
+        # Configure and build using the default preset
+        cmake --build build || exit 1
+
+        # Recompile the dynamic lib
+        echo -e "\033[0;34mBuilding Python extension with f2py...\033[0m"
+        cd src/python/NewCEA
+        f2py -c -m FCEA2 ../../fortran/lib/CEAinc.f90 ../../fortran/lib/cea2.f
+        mv FCEA2* FCEA2.so
+        ;;
+    setvars)
+        log "Setting project environment variables"
+        define_path
+        ;;
+    *)
+        echo "Error: Unknown command '$COMMAND'"
+        usage
+        ;;
+esac
